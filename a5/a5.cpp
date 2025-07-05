6f5a97d8a2978df5f58ba802704bbb18c02855df
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2025-07-05 19:40:58 -0300

    example of const and multiple declaration

---
+++ a5/a5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.14
+
+int main(){
+    //int vidas;
+    //int tiros;
+    //int hp;
+    
+    int vidas = 0, tiros = 0, hp = 0;
+    
+    std::cout << "Vidas: " << vidas << "Tiros: " << tiros << "HP: " << hp << endl;
+    std::cout << "O numero pi é: " << pi;
+    return 0;
+}
